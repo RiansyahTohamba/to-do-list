e8f3e2d840f6040c8765aa725b7b2fca095bc239
muh riansyah <mriansyah93@gmail.com>
2019-09-18 04:01:51 +0700

    remove url

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,10 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from lists import views
-from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('', views.home_page, name='home'),
-    path('admin/', admin.site.urls),
 ]
