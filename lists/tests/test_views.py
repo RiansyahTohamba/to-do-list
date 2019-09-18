53b5e9ec6346cf9d6d6fb35cf5bbf7b64287ea56
muh riansyah <mriansyah93@gmail.com>
2019-09-18 07:57:19 +0700

    set test for homepage

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.http import HttpRequest
 from django.urls import resolve
-from django.test import TestCase,Client
+from django.test import TestCase
 from lists.views import home_page
 
 
@@ -8,10 +8,16 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
-        
+
     def test_home_page_returns_correct_html(self):
         request = HttpRequest() #
         response = home_page(request) #
         self.assertTrue(response.content.startswith(b'<html>')) #
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertIn(b'<title>Homepage</title>', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))    
+
+    def test_home_page_contains_owner_name(self):
+        request = HttpRequest() #
+        response = home_page(request) #
+        content = response.content.strip().decode()
+        self.assertIn('Muhammad Riansyah Tohamba', content)
