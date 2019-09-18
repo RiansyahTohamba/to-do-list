d1545c10055f45c5b17fd68660eb7033b2a6844a
muh riansyah <mriansyah93@gmail.com>
2019-09-18 08:45:33 +0700

    change

--- superlists/settings.py
+++ superlists/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xox%m)t)y$2klzszre5vqdh2)s_-a1zmpj1nz51y0%$8ioj!(4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
