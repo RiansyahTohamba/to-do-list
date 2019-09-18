94ee412d07c34ad8d4064ffb3a511fdc6754dccb
muh riansyah <mriansyah93@gmail.com>
2019-09-18 08:01:37 +0700

    write implementation to pass test

--- lists/views.py
+++ lists/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-	return HttpResponse('<html><title>To-Do lists</title></html>')
+	return HttpResponse('<html><title>Homepage</title><body> <h1>Muhammad Riansyah Tohamba<h1></body></html>')
