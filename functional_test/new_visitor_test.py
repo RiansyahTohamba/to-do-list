dbedd01e7f9af353716aa97296c721398d19e1c9
muh riansyah <mriansyah93@gmail.com>
2019-09-24 08:40:44 +0700

    change browser

--- functional_test/new_visitor_test.py
+++ functional_test/new_visitor_test.py
@@ -4,8 +4,18 @@ from django.test import LiveServerTestCase
 class NewVisitorTest(LiveServerTestCase):
 #
 	def setUp(self): #
-		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
+        options = Options()
+        options.add_argument('--dns-prefetch-disable')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--headless')
+        options.add_argument('disable-gpu')
+        try:
+            self.browser = webdriver.Chrome('./chromedriver.exe',
+                options=options)
+        except:
+            self.browser = webdriver.Chrome('./chromedriver',
+                options=options)
+        self.browser.implicitly_wait(3)
 
 	def tearDown(self): #
 		self.browser.quit()
