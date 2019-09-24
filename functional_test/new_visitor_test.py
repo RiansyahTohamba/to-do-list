24830457d5e9eee5c4acfa885c24f8396dd668f3
muh riansyah <mriansyah93@gmail.com>
2019-09-24 08:47:18 +0700

    fix indetation;

--- functional_test/new_visitor_test.py
+++ functional_test/new_visitor_test.py
@@ -4,18 +4,17 @@ from django.test import LiveServerTestCase
 class NewVisitorTest(LiveServerTestCase):
 #
 	def setUp(self): #
-        options = Options()
-        options.add_argument('--dns-prefetch-disable')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--headless')
-        options.add_argument('disable-gpu')
-        try:
-            self.browser = webdriver.Chrome('./chromedriver.exe',
-                options=options)
-        except:
-            self.browser = webdriver.Chrome('./chromedriver',
-                options=options)
-        self.browser.implicitly_wait(3)
+		options = Options()
+		options.add_argument('--dns-prefetch-disable')
+		options.add_argument('--no-sandbox')
+		options.add_argument('--headless')
+		options.add_argument('disable-gpu')
+		try:
+			self.browser = webdriver.Chrome('./chromedriver.exe',options=options)
+		except:
+			self.browser = webdriver.Chrome('./chromedriver',options=options)
+			
+		self.browser.implicitly_wait(3)
 
 	def tearDown(self): #
 		self.browser.quit()
