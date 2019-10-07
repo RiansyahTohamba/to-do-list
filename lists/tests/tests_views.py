6a3f8878eb7705489a47ff0d65394962d532221a
muhammad riansyah <mriansyah93@gmail.com>
2019-10-07 17:21:33 +0700

    rename unit test file

--- lists/tests/tests_views.py
+++ lists/tests/tests_views.py
@@ -41,7 +41,7 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_displays_all_list_items(self):
         Item.objects.create(text='itemey 1')
