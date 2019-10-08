582a4fe494da41b48e8aef7ac6daf58713a7152e
muhammad riansyah <mriansyah93@gmail.com>
2019-10-08 10:02:04 +0700

    remove $ from url

--- superlists/urls.py
+++ superlists/urls.py
@@ -20,5 +20,5 @@ from lists import urls as list_urls
 
 urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
-    url(r'^lists/$', include(list_urls)),
+    url(r'^lists/', include(list_urls)),
 ]
