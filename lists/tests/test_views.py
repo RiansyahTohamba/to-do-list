50ea47b34b053c54482e2cf827f7167b2021ca86
muhammad riansyah <mriansyah93@gmail.com>
2019-10-08 07:57:28 +0700

    adjust models so that items are associated with different list

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.http import HttpRequest
 from django.urls import resolve
 from django.test import TestCase
-from lists.models import Item
+from lists.models import Item,List
 from lists.views import *
 
 
@@ -11,6 +11,7 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
+        list_ = List.objects.create()
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
         response = self.client.get('/lists/the-only-list-in-the-world/')
@@ -83,21 +84,3 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertIn('oh tidak', personal_comment(Item.objects.all()))
 
-class ItemModelTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.save()
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(second_saved_item.text, 'Item the second')
