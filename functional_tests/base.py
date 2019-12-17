1a3e38ae3853652d28027cbb7822c40d0552f334
muhammad riansyah <mriansyah93@gmail.com>
2019-12-17 17:29:38 +0700

    fix indentation ft/base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -7,6 +7,8 @@ from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
 
+from .management.commands.create_session import create_pre_authenticated_session
+
 MAX_WAIT = 10
 
 def wait(fn):
@@ -59,3 +61,16 @@ class FunctionalTest(StaticLiveServerTestCase):
 		self.browser.find_element_by_name('email')
 		navbar = self.browser.find_element_by_css_selector('.navbar')
 		self.assertNotIn(email, navbar.text)
+
+	def create_pre_authenticated_session(self, email):
+		session_key = create_pre_authenticated_session(email)
+
+		## to set a cookie we need to first visit the domain.
+		## 404 pages load the quickest!
+		self.browser.get(self.live_server_url + "/404_no_such_url/")
+		self.browser.add_cookie(dict(
+		    name=settings.SESSION_COOKIE_NAME,
+		    value=session_key,
+		    path='/',
+		))
+
