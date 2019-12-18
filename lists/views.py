41637a50a19171f9f9741240d7b05af853ef31ad
muhammad riansyah <mriansyah93@gmail.com>
2019-12-18 13:24:18 +0700

    remove add_item on lists/views.py

--- lists/views.py
+++ lists/views.py
@@ -25,12 +25,6 @@ def home_page(request):
         'form':ItemForm()
     })
 
-def add_item(request,list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect(f'/lists/{list_.id}/')
-
-
 def new_list(request):
     form = NewListForm(data=request.POST)
     if form.is_valid():
