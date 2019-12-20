3e20b4d4199e32f82e27d83a5fc48b01a69900f4
muhammad riansyah <mriansyah93@gmail.com>
2019-12-20 16:18:48 +0700

    added FT for checking duplicate items

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,44 +1,20 @@
 from selenium.webdriver.common.keys import Keys
+from lists.forms import DUPLICATE_ITEM_ERROR
 from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
-	def test_cannot_add_empty_list_items(self):
+	def test_cannot_add_duplicate_item(self):
 		# Edith goes to the home page and accidentally tries to submit
-		# an empty list item. She hits Enter on the empty input box
+		# an duplicate list item.
 		self.browser.get(self.live_server_url)
-		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
-
-		# The home page refreshes, and there is an error message saying
-		# that list items cannot be blank
-		self.assertEqual(
-		    self.browser.find_element_by_css_selector('.has-error').text,  
-		    "You can't have an empty list item"  
-		)
-
-		# She tries again with some text for the item, which now works
-		self.fail('finish this test!')
-		# The home page refreshes, and there is an error message saying
-		# that list items cannot be blank
-		self.wait_for(lambda: self.assertEqual(  
-		    self.browser.find_element_by_css_selector('.has-error').text,
-		    "You can't have an empty list item"
-		))		
-		
-		# She tries again with some text for the item, which now works
 		self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
 		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 		self.wait_for_row_in_list_table('1: Buy milk')
-
-		# Perversely, she now decides to submit a second blank list item
+		self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
 		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
-
-		# She receives a similar warning on the list page
-		self.wait_for(lambda: self.assertEqual(
+		# The home page refreshes, and there is an error message saying
+		# that list items cannot be duplicated
+		self.wait_for(lambda: self.assertEqual(  
 		    self.browser.find_element_by_css_selector('.has-error').text,
-		    "You can't have an empty list item"
-		))
-		# And she can correct it by filling some text in
-		self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
-		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
-		self.wait_for_row_in_list_table('1: Buy milk')
-		self.wait_for_row_in_list_table('2: Make tea')		
+		    DUPLICATE_ITEM_ERROR
+		))	
