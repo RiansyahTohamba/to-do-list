57e58a57b09ce579ad8cdae04ff4488067826624
muhammad riansyah <mriansyah93@gmail.com>
2019-12-23 15:54:01 +0700

    add element btn submit

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -35,10 +35,13 @@ class FunctionalTest(StaticLiveServerTestCase):
 	def get_item_input_box(self):
 		return self.browser.find_element_by_id('id_new_item')
 
+	def get_btn_submit(self):
+		return self.browser.find_element_by_id('btn_submit')
+
 	def add_list_item(self, item_text):
 		num_rows = len(self.browser.find_elements_by_css_selector('#id_list_table tr'))
 		self.get_item_input_box().send_keys(item_text)
-		self.get_item_input_box().send_keys(Keys.ENTER)
+		self.get_btn_submit().send_keys(Keys.ENTER)
 		item_number = num_rows + 1
 		self.wait_for_row_in_list_table(f'{item_number}: {item_text}')
 		
@@ -49,7 +52,7 @@ class FunctionalTest(StaticLiveServerTestCase):
 	@wait
 	def wait_for_row_in_list_table(self, row_text):
 		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
+		rows = table.find_elements_by_class_name('item_text')
 		self.assertIn(row_text, [row.text for row in rows])
 
 	@wait
